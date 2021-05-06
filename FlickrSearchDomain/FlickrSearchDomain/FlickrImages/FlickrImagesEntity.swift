280ea48a4169d24469c0fd3d7e021be80443da59
Mohammed Abdelaty <dev.mohgamal@gmail.com>
2021-05-06 23:02:29 +0200

    adding FlickrImagesEntity

---
+++ FlickrSearchDomain/FlickrSearchDomain/FlickrImages/FlickrImagesEntity.swift
@@ -0,0 +1,59 @@
+//
+//  FlickrImagesEntity.swift
+//  FlickrSearchDomain
+//
+//  Created by Mohammed Abd El-Aty on 06/05/2021.
+//
+
+import Foundation
+
+public struct FlickrImagesEntity: Codable {
+    public let photos: Photos?
+    public let stat: String?
+
+    public init(photos: Photos?, stat: String?) {
+        self.photos = photos
+        self.stat = stat
+    }
+
+    public struct Photos: Codable {
+        public let page: Int?
+        public let pages: Int?
+        public let perpage: Int?
+        public let total: String?
+        public let photo: [Photo]?
+
+        public init(page: Int?, pages: Int?, perpage: Int?, total: String?, photo: [Photo]?) {
+            self.page = page
+            self.pages = pages
+            self.perpage = perpage
+            self.total = total
+            self.photo = photo
+        }
+    }
+
+    public struct Photo: Codable {
+        public let id: String?
+        public let owner: String?
+        public let secret: String?
+        public let server: String?
+        public let farm: Int?
+        public let title: String?
+        public let ispublic: Int?
+        public let isfriend: Int?
+        public let isfamily: Int?
+
+        public init(id: String?, owner: String?, secret: String?, server: String?,
+                    farm: Int?, title: String?, ispublic: Int?, isfriend: Int?, isfamily: Int?) {
+            self.id = id
+            self.owner = owner
+            self.secret = secret
+            self.server = server
+            self.farm = farm
+            self.title = title
+            self.ispublic = ispublic
+            self.isfriend = isfriend
+            self.isfamily = isfamily
+        }
+    }
+}
