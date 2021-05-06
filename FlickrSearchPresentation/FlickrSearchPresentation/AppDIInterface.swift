1760e943c9cfa2124bcbac136825e2a5a6e5fbe9
Mohammed Abdelaty <dev.mohgamal@gmail.com>
2021-05-07 00:10:30 +0200

    adding AppDIInterface

---
+++ FlickrSearchPresentation/FlickrSearchPresentation/AppDIInterface.swift
@@ -0,0 +1,12 @@
+//
+//  AppDIInterface.swift
+//  FlickrSearchPresentation
+//
+//  Created by Mohammed Abd El-Aty on 07/05/2021.
+//
+
+import Foundation
+
+public protocol AppDIInterface {
+    func flickrImagesSearchDependencies() -> FlickrImagesSearchVM
+}
---
+++ FlickrSearchPresentation/FlickrSearchPresentation/FlickrImagesSearch/FlickrImagesSearchVM.swift
@@ -0,0 +1,12 @@
+//
+//  FlickrImagesSearchVM.swift
+//  FlickrSearchPresentation
+//
+//  Created by Mohammed Abd El-Aty on 07/05/2021.
+//
+
+import Foundation
+
+public class FlickrImagesSearchVM {
+    
+}
