99b65cc3e7e6311f623d9c71983c8522b70102b0
Mohammed Abdelaty <dev.mohgamal@gmail.com>
2021-05-07 00:12:12 +0200

    adding empty flickrImage collection view cell

---
+++ FlickrSearchPresentation/FlickrSearchPresentation/FlickrImagesSearch/CustomCells/FlickrImageCell.swift
@@ -0,0 +1,19 @@
+//
+//  FlickrImageCell.swift
+//  FlickrSearchPresentation
+//
+//  Created by Mohammed Abd El-Aty on 07/05/2021.
+//
+
+import UIKit
+
+class FlickrImageCell: UICollectionViewCell {
+
+    static let cellIdentifier = "flickrImageCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
