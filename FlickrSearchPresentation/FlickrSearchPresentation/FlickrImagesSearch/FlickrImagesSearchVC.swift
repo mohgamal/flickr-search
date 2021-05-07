859866c98308bafeef516cca3df32485edc308f2
Mohammed Abdelaty <dev.mohgamal@gmail.com>
2021-05-07 08:41:53 +0200

    adding FlickrImagesSearchVC

---
+++ FlickrSearchPresentation/FlickrSearchPresentation/FlickrImagesSearch/FlickrImagesSearchVC.swift
@@ -0,0 +1,29 @@
+//
+//  FlickrImagesSearchVC.swift
+//  FlickrSearchPresentation
+//
+//  Created by Mohammed Abd El-Aty on 07/05/2021.
+//
+
+import UIKit
+
+class FlickrImagesSearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
